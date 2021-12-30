b9cf5a90206cdeae83b2c227052fe591d09217f0
Billy-Hu <billy.hu@applovin.com>
2021-12-30 11:59:15 -0800

    Update HomeScreen.cs

--- DemoApp/Assets/Scripts/HomeScreen.cs
+++ DemoApp/Assets/Scripts/HomeScreen.cs
@@ -487,7 +487,7 @@ public class HomeScreen : MonoBehaviour
         double revenue = adInfo.Revenue;
         
         // Miscellaneous data
-        string countryCode = MaxSdk.GetSdkConfiguration().CountryCode; // "US" for the United States, etc - Note: Do not confuse this with currency code which is "USD" in most cases!
+        string countryCode = MaxSdk.GetSdkConfiguration().CountryCode; // "US" for the United States, etc - Note: Do not confuse this with currency code which is "USD"!
         string networkName = adInfo.NetworkName; // Display name of the network that showed the ad (e.g. "AdColony")
         string adUnitIdentifier = adInfo.AdUnitIdentifier; // The MAX Ad Unit ID
         string placement = adInfo.Placement; // The placement this ad's postbacks are tied to
