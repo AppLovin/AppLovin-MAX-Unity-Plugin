679b17a19c0d9b3af7c1f99ff627e4022744645f
AppLovin-Mobile-Engineering <jenkins@mobile-jenkins-slave1.dev-alcfd.com>
2025-09-03 12:39:09 -0700

    demoapp/bump_applovin_plugin_8.4.0

--- DemoApp/Assets/MaxSdk/AppLovin/Plugins/iOS/MAUnityPlugin.mm
+++ DemoApp/Assets/MaxSdk/AppLovin/Plugins/iOS/MAUnityPlugin.mm
@@ -8,7 +8,7 @@
 
 #import "MAUnityAdManager.h"
 
-#define VERSION @"8.3.1"
+#define VERSION @"8.4.0"
 #define NSSTRING(_X) ( (_X != NULL) ? [NSString stringWithCString: _X encoding: NSStringEncodingConversionAllowLossy].al_stringByTrimmingWhitespace : nil)
 
 @interface NSString (ALUtils)
--- DemoApp/Assets/MaxSdk/Scripts/MaxSdk.cs
+++ DemoApp/Assets/MaxSdk/Scripts/MaxSdk.cs
@@ -14,7 +14,7 @@ public class MaxSdk :
     MaxSdkUnityEditor
 #endif
 {
-    private const string _version = "8.3.1";
+    private const string _version = "8.4.0";
 
     /// <summary>
     /// Returns the current plugin version.
